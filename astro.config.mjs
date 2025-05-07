b7ba5edac1c34bd2bf1bf3c03c17dd43f5abeae5
Anton Nageh <anton.nageh20@gmail.com>
2025-05-07 23:03:17 +0300

    FinishedFirstSection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import flowbiteReact from "flowbite-react/plugin/astro";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+	},
+
+  integrations: [react(), flowbiteReact()],
+});
